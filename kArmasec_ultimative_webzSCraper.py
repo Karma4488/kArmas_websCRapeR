3fcb1e835dd20126efe378186c0c2b3383efd1af
ƙArma <heremailscome@proton.me>
2025-11-09 13:48:38 +0100

    Update kArmasec_ultimative_webzSCraper.py

--- kArmasec_ultimative_webzSCraper.py
+++ kArmasec_ultimative_webzSCraper.py
@@ -12,7 +12,7 @@ import sys
 import os
 
 # Konfiguration
-BASE_URL = "https://btc-net.bg"
+BASE_URL = "https://yourtarget.com"
 OUTPUT_DIR = "index.html"
 USER_AGENT = ("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 "
               "(KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
